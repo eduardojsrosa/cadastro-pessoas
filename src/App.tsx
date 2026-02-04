f46d50cdbe412e21ec3d1de5644be0950e6fafdd
Eduardo Júnior Scheibner Rosa <EduardoRosa@DOMINIO.DTC>
2026-02-04 18:11:40 -0300

    Projeto inicial React + TypeScript + Ant Design

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "antd"
+
+export default function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Cadastro de Pessoas</h1>
+      <Button type="primary">Botão Ant Design</Button>
+    </div>
+  )
+}
