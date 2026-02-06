11313ffd3238abe982e08a6b69739b3307ccd48d
Eduardo Rosa <eduardoscheibner@hotmail.com>
2026-02-05 23:35:40 -0300

    Excluir registro

--- src/App.tsx
+++ src/App.tsx
@@ -10,23 +10,28 @@ const App = () => {
   const [form] = Form.useForm();
 
   const [listaPessoas, setListaPessoas] = useState<Pessoa[]>([
-    { nome: 'Eduardo', email: 'teste@teste.com' },
-    { nome: 'Camila', email: 'camila@teste.com' },
+    { id: 1, nome: 'Eduardo', email: 'teste@teste.com' },
+    { id: 2, nome: 'Camila', email: 'camila@teste.com' },
   ]);
 
   const handleSubmit = (values: Pessoa) => {
-    setListaPessoas([
-      ...listaPessoas, // clone da lista
-      values // add objeto novo
-    ]);  
+    // Verificar o próximo ID. Default é 1 ou maior ID da lista +1
+    const nextId =
+      listaPessoas.length === 0
+        ? 1
+        : Math.max(...listaPessoas.map(p => Number(p.id))) + 1;
+
+    setListaPessoas(
+      [...listaPessoas, {...values, id: nextId}]
+    );
 
     form.resetFields();
   }
 
-  const deletePessoa = (email: string) => {
+  const deletePessoa = (id: number) => {
     // O filter vai retornar um novo array removendo o item
     setListaPessoas(
-      listaPessoas.filter(item => item.email !== email)
+      listaPessoas.filter(item => item.id !== id)
     );
   }
 
@@ -34,6 +39,11 @@ const App = () => {
   // dataIndex = campo / nome do objeto
   // key é necessário devido ao map
   const columns: TableProps<Pessoa>['columns'] = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      key: 'id',
+    },
     {
       title: 'Nome',
       dataIndex: 'nome',
@@ -61,7 +71,7 @@ const App = () => {
               danger 
               icon={<DeleteOutlined />}
               shape="circle"
-              onClick={() => deletePessoa(record.email)}
+              onClick={() => deletePessoa(record.id)}
             />
           </Tooltip>
         </Space>
@@ -123,7 +133,7 @@ const App = () => {
         </Form>
       </Card>
 
-      <Table dataSource={listaPessoas} columns={columns} rowKey="email" />
+      <Table dataSource={listaPessoas} columns={columns} rowKey="id" />
     </div>  
   );
 };
